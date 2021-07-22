2f8c5a721e022b6f26f116f1c455677339bd1397
Taejin Jung <taejin5314@gmail.com>
2021-07-22 19:58:42 +0000

    jquery functions

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -1,10 +1,25 @@
 $(() => {
-  $('.menu-resume-button').on('click', (event) => {
-    $('.modal').fadeOut();
+  // when user clicks menu button during the game
+  $('.nav-left button').on('click', (event) => {
+    $('.modal').fadeIn();
   })
 
+  // when user clicks resume button
+  $('.menu-resume-button').on('click', (event) => {
+    $('.modal').fadeOut();
+    $('.main-content').css('visibility', 'visible')
+  })
+
+  // when user clicks new game button
   $('.menu-new-game-button').on('click', (event) => {
     $('.menu-container').fadeOut()
     $('.new-game-board').css('display', 'flex').fadeIn()
+  })
+
+  $('.new-game-board-start').on('click', (event) => {
+    $('.new-game-board').fadeOut();
+    $('.menu-container').fadeIn();
+    $('.modal').fadeOut();
+    $('.main-content').css('visibility', 'visible').fadeIn();
   })
 })
